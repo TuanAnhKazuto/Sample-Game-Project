165c1e5bbdf8f1c87fd3fd9e3b29164ed440f6f2
TuanAnhKazuto <tuananhkazuto@gmail.com>
2024-06-01 15:27:46 +0700

    Code quái di chuyển

--- Assets/_Scripts/Enemy/EnemyMove.cs
+++ Assets/_Scripts/Enemy/EnemyMove.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class EnemyMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpHight = 5f;
+    [SerializeField] private float left;
+    [SerializeField] private float right;
+    private int moveDiretion = 1;
+
+    private void Update()
     {
-        
+        transform.Translate(Vector2.up * jumpHight * Time.deltaTime);
+
+        transform.Translate(Vector2.right * moveSpeed * moveDiretion * Time.deltaTime);
+        Vector2 scale = transform.localScale;
+        if(transform.position.x <= left)
+        {
+            moveDiretion = 1;
+            scale.x = 1;
+        }
+        else if(transform.position.x >= right)
+        {
+            moveDiretion = -1;
+            scale.x = -1;
+        }
+        transform.localScale = scale;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
