78559a09ddc3290808da700739f57cfdeb08bba0
TuanAnhKazuto <tuananhkazuto@gmail.com>
2024-06-02 10:43:48 +0700

    Hoàn thành Scene1

---
+++ Assets/_Scripts/Boss/BossSlime.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossSlime : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public Transform player;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
