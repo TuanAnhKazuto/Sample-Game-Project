f62fcc22a2856167d60b8e80f7b2d5aebf135c8d
TuanAnhKazuto <tuananhkazuto@gmail.com>
2024-06-04 11:05:35 +0700

    viết code tấn công cho boss(chưa xong)

--- Assets/_Scripts/Boss/BossSlime.cs
+++ Assets/_Scripts/Boss/BossSlime.cs
@@ -4,13 +4,34 @@ using UnityEngine;
 
 public class BossSlime : MonoBehaviour
 {
-    Rigidbody2D rb;
-    public Transform player;
+    [SerializeField] private Transform bulletTransform;
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private float bullletSpeed = 5f;
+    private bool seenPlayer = false;
 
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if(collision.gameObject.tag == "Player")
+        {
+            seenPlayer = true;
+        }
+    }
+
+    public void BossAttack()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, bulletTransform.position, Quaternion.identity);
+        bullet.GetComponent<Rigidbody2D>().velocity = bulletTransform.right * bullletSpeed;
+
+    }
+
+    IEnumerator FireDelay()
+    {
+        do
+        {
+            BossAttack();
+            yield return new WaitForSeconds(0.3f);
+
+        } while (true);
     }
 }
